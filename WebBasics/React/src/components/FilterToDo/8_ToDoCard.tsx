3f48cf47954b7791bde1a96126fd1c537cfb52ec
nihat_08 <nihat.oezgoec@student.htldornbirn.at>
2026-01-27 12:07:23 +0100

    a

--- WebBasics/React/src/components/FilterToDo/8_ToDoCard.tsx
+++ WebBasics/React/src/components/FilterToDo/8_ToDoCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ToDoCard({id, title, completed}: Props) {
   return (
-    <div className={`border p-4 m-4 ${completed?"bg-green-200":"bg-red-200"}`}> // ? schaut ob completed true oder false ist
+    <div className={`border p-4 m-4 ${completed?"bg-green-200":"bg-red-200"}`}> 
         <div>{id}</div>
         <div>{title}</div>
         <div>{completed? "done":"open"}</div>
